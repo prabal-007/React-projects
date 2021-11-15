3eff96e4146e5f7d9335f25813322e79f4be6590
Starkk <42775517+prabal-007@users.noreply.github.com>
2021-11-15 23:48:24 +0530

    Create About.js

---
+++ Todo list/src/my components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is about component page!
+            <p>
+                The app is almost ready.
+            </p>
+        </div>
+    )
+}
